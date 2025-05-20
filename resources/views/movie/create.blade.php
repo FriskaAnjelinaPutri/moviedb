1a9b1dfa39e6cb97fcc969d6933d62529b3181c7
FriskaAnjelinaPutri <friskaenjelina80@gmail.com>
2025-05-20 09:26:16 +0700

    homepage

--- resources/views/movie/create.blade.php
+++ resources/views/movie/create.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.main')
-@section('title','Tambah Data Movie')
+
+@section('title','Tambah Movie')
 @section('navMovie','active')
+
 @section('content')
 <div class="row">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <div class="col-12">
             <h1 class="h2">Input Data Movie</h1>
-            <form action="{{ route('movie.store') }}" method="post">
+            <form action="{{ route('movie.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
 
                 <div class="row mb-3">
-                    <label for="title" class="col-sm-2 col-form-label">Judul</label>
+                    <label for="title" class="col-sm-2 col-form-label">Judul Movie</label>
                     <div class="col-sm-10">
                         <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" id="title" value="{{ old('title') }}">
                         @error('title')
@@ -19,20 +21,11 @@
                     </div>
                 </div>
 
-                <div class="row mb-3">
-                    <label for="slug" class="col-sm-2 col-form-label">Slug</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="slug" class="form-control @error('slug') is-invalid @enderror" id="slug" value="{{ old('slug') }}">
-                        @error('slug')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                </div>
-
+                <!-- Ubah dari 'description' menjadi 'synopsis' -->
                 <div class="row mb-3">
                     <label for="synopsis" class="col-sm-2 col-form-label">Sinopsis</label>
                     <div class="col-sm-10">
-                        <textarea name="synopsis" class="form-control @error('synopsis') is-invalid @enderror" id="synopsis" rows="5">{{ old('synopsis') }}</textarea>
+                        <textarea name="synopsis" class="form-control @error('synopsis') is-invalid @enderror" id="synopsis" rows="6">{{ old('synopsis') }}</textarea>
                         @error('synopsis')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
@@ -42,7 +35,14 @@
                 <div class="row mb-3">
                     <label for="category_id" class="col-sm-2 col-form-label">Kategori</label>
                     <div class="col-sm-10">
-                        <input type="number" name="category_id" class="form-control @error('category_id') is-invalid @enderror" id="category_id" value="{{ old('category_id') }}">
+                        <select name="category_id" id="category_id" class="form-select @error('category_id') is-invalid @enderror">
+                            <option value="">-- Pilih Kategori --</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>
+                                    {{ $category->category_name }}
+                                </option>
+                            @endforeach
+                        </select>
                         @error('category_id')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
@@ -52,7 +52,7 @@
                 <div class="row mb-3">
                     <label for="year" class="col-sm-2 col-form-label">Tahun</label>
                     <div class="col-sm-10">
-                        <input type="number" name="year" class="form-control @error('year') is-invalid @enderror" id="year" value="{{ old('year') }}">
+                        <input type="number" name="year" class="form-control @error('year') is-invalid @enderror" id="year" value="{{ old('year') }}" min="1900" max="{{ date('Y') }}">
                         @error('year')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
@@ -60,9 +60,9 @@
                 </div>
 
                 <div class="row mb-3">
-                    <label for="actors" class="col-sm-2 col-form-label">Pemeran</label>
+                    <label for="actors" class="col-sm-2 col-form-label">Aktor</label>
                     <div class="col-sm-10">
-                        <textarea name="actors" class="form-control @error('actors') is-invalid @enderror" id="actors" rows="3">{{ old('actors') }}</textarea>
+                        <input type="text" name="actors" class="form-control @error('actors') is-invalid @enderror" id="actors" value="{{ old('actors') }}">
                         @error('actors')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
@@ -70,9 +70,9 @@
                 </div>
 
                 <div class="row mb-3">
-                    <label for="cover_image" class="col-sm-2 col-form-label">Gambar Sampul</label>
+                    <label for="cover_image" class="col-sm-2 col-form-label">Cover Image</label>
                     <div class="col-sm-10">
-                        <input type="text" name="cover_image" class="form-control @error('cover_image') is-invalid @enderror" id="cover_image" value="{{ old('cover_image') }}">
+                        <input type="file" name="cover_image" class="form-control @error('cover_image') is-invalid @enderror" id="cover_image" accept="image/*">
                         @error('cover_image')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MovieController::class, 'homepage']);
 
-Route::resource('/movie', MovieController::class);
-Route::resource('/category',CategoryController::class);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::resource('/movie', MovieController::class);
+// Route::resource('/category',CategoryController::class);
