57264d70fd7b2c237a150989c021115c48d67a4e
FriskaAnjelinaPutri <friskaenjelina80@gmail.com>
2025-06-03 09:28:42 +0700

    login logout

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(5)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(CategorySeeder::class);
-        Movie::factory()->count(15)->create();
+        // $this->call(CategorySeeder::class);
+        // Movie::factory()->count(15)->create();
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
@@ -7,5 +8,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [MovieController::class, 'homepage']);
 Route::get('movie/{id}/{slug}', [MovieController::class, 'detailmovie']);
 
-Route::get('create-movie', [MovieController::class, 'create'])->name('createMovie');
+Route::get('create-movie', [MovieController::class, 'create'])->name('createMovie')->middleware('auth');
 Route::post('/movie', [MovieController::class, 'store'])->name('storeMovie');
+
+Route::get('/login', [AuthController::class, 'loginform'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/logout', [AuthController::class, 'logout'])-> name('logout');
